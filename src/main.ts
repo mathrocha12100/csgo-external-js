0a2dc242e3837dbe9aa8a8d530002163416507a1
João Regis <joao.reegiis@gmail.com>
2021-10-07 13:09:54 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,30 @@ import RCS from './features/rcs';
 import Triggerbot from './features/triggerbot';
 import BunnyHop from './features/bunnyhop';
 import Aimbot from './features/aimbot';
-
 import aks from 'asynckeystate';
-
 import { keys } from './utils/keys';
-
-import { getMem } from './functions/mem';
+import { getMem, sleep } from './functions/mem';
 import { canRun } from './utils/config';
 
-function execute() {
-    const { clientModule, engineModule } = getMem();
+(async function () {
+ 
+    let { success, clientModule, engineModule } = getMem();
+
+    // while (!success) {
+    //     const proc = getMem();
+    //     if (proc.success) {
+    //         success = proc.success;
+    //         clientModule = proc.clientModule;
+    //         engineModule = proc.engineModule;
+    //         console.log("Loading modules and starting cheat.");
+    //     } else {
+    //         console.log("Awaiting csgo.exe process show up.");
+    //     }
+
+    //     await sleep(2000);
+    // }
 
     let stop = false;
-
     while (!stop) {
         if (canRun('aimbot')) Aimbot(engineModule);
         if (canRun('glow')) Glow(clientModule);
@@ -29,6 +40,5 @@ function execute() {
     }
 
     process.exit(1);
-}
-
-execute();
+    
+})();
